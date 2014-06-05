ce669e86609ac6060950dadbafe209fc00405087
João Lopes <joaopclopes@gmail.com>
2014-06-05 16:36:02 +0100

    Added new single session protocols to the dictionary

--- Launcher.java
+++ Launcher.java
@@ -16,7 +16,7 @@ import jade.wrapper.StaleProxyException;
  */
 public class Launcher {
 	
-	private static Runtime runtime;
+	private static jade.core.Runtime runtime;
 	private static jade.wrapper.AgentContainer container;
 	private static Launcher instance;
 	private static String property_gui = "true";
@@ -26,29 +26,16 @@ public class Launcher {
 	
 	
 	public static void main(String[] args) {
-		setupPlatform();
 		runtime = jade.core.Runtime.instance();
 		Profile profile = new ProfileImpl();
 		profile.setParameter("gui", property_gui);
 		profile.setParameter("port", property_port);
 		profile.setParameter("jade_domain_df_maxresult", property_dx_maxresult );
 		container = runtime.createMainContainer(profile);
-		instance = new MyLauncher;
+		instance = new Launcher();
 		instance.setup();
 	}
-	
-	/**
-	 * Called by the main before creating the platform.
-	 * Use the gettters and setters to change the values of "gui", "port"
-	 * and "jade_domain_df_maxresult".
-	 */
-	private static void setupPlatform() {}
 
-	/**
-	 * Called by the main method. Override this method.
-	 */
-	protected void setup() {}
-	
 	/**
 	 * Agents are added to the AMS service and are
 	 * given and AID. 
